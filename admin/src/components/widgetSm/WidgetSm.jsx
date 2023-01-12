9d70c474522d1d907d08a2acd95e8305d2f9ae9e
lolxd <65647597+pnxmyth@users.noreply.github.com>
2023-01-12 22:31:25 +0530

    last commit

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -9,7 +9,7 @@ export default function WidgetSm() {
   useEffect(() => {
     const getNewUsers = async () => {
       try {
-        const res = await axios.get("http://localhost:8800/api/users?new=true", {
+        const res = await axios.get("https://ntflx-clone-dmat.vercel.app/api/users?new=true", {
           headers: {
             token:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYjFlZTdhYTVhNWM1MmU4NDZjYzc4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3Mjg1NTQ2OCwiZXhwIjoxNjczMjg3NDY4fQ.LO0xvisxOo1_wDG4yEYbe8akgKeExjazY4w9_Ky4_qE",
