4fb5c1944281b78bb12c7867c91a566758825705
lolxd <65647597+pnxmyth@users.noreply.github.com>
2023-01-12 22:45:43 +0530

    token add final

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -12,7 +12,7 @@ export default function WidgetSm() {
         const res = await axios.get("https://ntflx-clone-dmat.vercel.app/api/users?new=true", {
           headers: {
             token:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYjFlZTdhYTVhNWM1MmU4NDZjYzc4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3Mjg1NTQ2OCwiZXhwIjoxNjczMjg3NDY4fQ.LO0xvisxOo1_wDG4yEYbe8akgKeExjazY4w9_Ky4_qE",
+              "Bearer " + JSON.parse(localStorage.getItem("user")).accesstoken,
           },
         });
         setNewUsers(res.data);
